dd274328fbda0852bb0469c85868b727f172a2ca
moora10 <52221848+moora10@users.noreply.github.com>
2020-06-16 14:04:39 -0400

    Added predict button and download functionality

--- Amyloid_augury_for_cohorts.py
+++ Amyloid_augury_for_cohorts.py
@@ -30,20 +30,34 @@ import pickle
 filename = 'random_forest_model_optimized.sav'
 loaded_model = pickle.load(open(filename, 'rb'))
 
-out = loaded_model.predict(data)
+if st.button('Predict'):
+	out = loaded_model.predict(data)
+	#save prediction as an array 
+	out = np.array(out)
 
-#save prediction as an array 
-out = np.array(out)
+	out_framed = pd.DataFrame(data=out.flatten(), columns=['Prediction'])
 
-out_framed = pd.DataFrame(data=out.flatten(), columns=['Prediction'])
+	#change from 0 and 1 to high and low risk
+	out_framed.replace(0, "high risk", inplace=True)
+	out_framed.replace(1, "low risk", inplace=True)
 
-out_framed.replace(0, "high risk", inplace=True)
-out_framed.replace(1, "low risk", inplace=True)
-#out_framed.replace(to_replace =[0],  
-                          #  value ='high risk')
-#append the array onto the dataframe as a new column
-#data['prediction'] = out
-results_full = pd.concat([out_framed, data], axis=1, sort=False)
+	results_full = pd.concat([out_framed, data], axis=1, sort=False)
 
-st.write(results_full)
+	st.write(results_full)
+	
+	#create download button
+	import base64
+	def get_table_download_link(df):
+	    """Generates a link allowing the data in a given panda dataframe to be downloaded
+	    in: dataframe
+	    out: href string
+	    reference:https://discuss.streamlit.io/t/how-to-set-file-download-function/2141
+	    """
+	    csv = df.to_csv(index=False)
+	    b64 = base64.b64encode(
+		csv.encode()
+	    ).decode() # some strings <-> bytes conversions necessary here
+	    return f'<a href="data:file/csv;base64,{b64}" download="amyloid_risk_predictions.csv">Download csv file</a>'
+	
+	st.markdown(get_table_download_link(results_full), unsafe_allow_html=True)
 
