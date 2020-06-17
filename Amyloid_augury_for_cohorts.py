2e01d822285c00ae6a7e68a81d99c44515f4be73
moora10 <52221848+moora10@users.noreply.github.com>
2020-06-17 19:37:58 -0400

    fixed sidebar indentation

--- Amyloid_augury_for_cohorts.py
+++ Amyloid_augury_for_cohorts.py
@@ -78,8 +78,8 @@ if st.button('Predict'):
 	st.markdown(get_table_download_link(results_full), unsafe_allow_html=True)
 
 #### Allow exploration of individual candidates 
-    candidate_selection = st.sidebar.selectbox("Select a trial candidate for exploration",('Candidate 1', 'Candidate 2', 'Candidate 3', 'Candidate 4'))
-    if candidate_selection == 'Candidate 1':
+    	candidate_selection = st.sidebar.selectbox("Select a trial candidate for exploration",('Candidate 1', 'Candidate 2', 'Candidate 3', 'Candidate 4'))
+    	if candidate_selection == 'Candidate 1':
             st.write(results_full.iloc[0, 0:51])
             if results_full.iloc[0,0] == "high risk":
                 st.header("High risk for brain amyloid, recommended that this candidate moves to PET screen")
