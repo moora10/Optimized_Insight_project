2023908ab09e9c5c2a9c1fa7848cd193e004fab9
moora10 <52221848+moora10@users.noreply.github.com>
2020-06-17 18:12:04 -0400

    Added template and candidate exploration
    Addition of downloadable, empty .csv file which can be filled with candidate data. Also added a dropdown sidebar.

--- Amyloid_augury_for_cohorts.py
+++ Amyloid_augury_for_cohorts.py
@@ -12,11 +12,28 @@ import pandas as pd
 import numpy as np
 from sklearn import datasets, ensemble
 from sklearn.ensemble import RandomForestClassifier
+import base64
+import pickle 
 
 st.write("""
          # Amyloid Augury
          **_Personalized_** risk profiling for the earliest pathological marker of Alzheimer's disease
 """)
+
+#create template for download
+template = pd.read_csv('columns.csv')
+st.write('Template available for download below')
+def get_table_download_link(df):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(
+        csv.encode()
+    ).decode()  # some strings <-> bytes conversions necessary here
+    return f'<a href="data:file/csv;base64,{b64}" download="template.csv">Download template</a>'
+st.markdown(get_table_download_link(template), unsafe_allow_html=True)
 
 uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
 if uploaded_file is not None:
@@ -26,7 +43,6 @@ if uploaded_file is None:
      st.write("Please upload data file")
 
 #import model 
-import pickle
 filename = 'random_forest_model_optimized.sav'
 loaded_model = pickle.load(open(filename, 'rb'))
 
@@ -61,3 +77,15 @@ if st.button('Predict'):
 	
 	st.markdown(get_table_download_link(results_full), unsafe_allow_html=True)
 
+#### Allow exploration of individual candidates 
+    candidate_selection = st.sidebar.selectbox(
+    "Select a trial candidate for exploration",
+    ('Candidate 1', 'Candidate 2', 'Candidate 3', 'Candidate 4')
+)
+    if candidate_selection == 'Candidate 1':
+            st.write(results_full.iloc[0, 0:51])
+            if results_full.iloc[0,0] == "high risk":
+                st.header("High risk for brain amyloid, recommended that this candidate moves to PET screen")
+            else:
+                st.header("Low risk for brain amyloid, recommended that this candidate does not move to PET screen")
+   
